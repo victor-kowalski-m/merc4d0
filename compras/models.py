1d699d0e089512cb88769fbf271f2661e5d66f1f
Victor Kowalski Martins <victor.kowalski.m@gmail.com>
2020-12-12 20:31:07 -0300

    Nome foto e model img

--- compras/models.py
+++ compras/models.py
@@ -46,6 +46,7 @@ class Cartao(models.Model):
 
 class Produto(models.Model):
     nome = models.CharField(max_length=50)
+    img_url = models.URLField(max_length=1000, blank=True)
 
     def __str__(self):
         return f"{self.nome}"
